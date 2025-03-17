3de503a61a1b95cbed5f16d8a64b9c44419d7043
Radhika Rungta <166164321+radhikarungta11@users.noreply.github.com>
2025-03-17 15:13:01 +0530

    Update Payments.js

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -76,14 +76,7 @@ exports.capturePayment = async (req, res) => {
                 amount:paymentResponse.amount,
             });
         }
-        catch(error) {
-            console.error(error);
-            return res.status(500).json({
-                success:false,
-                message:error.message,
-            });
-        }
-    }
+       
     catch(error) {
         console.error(error);
         return res.status(500).json({
@@ -97,33 +90,29 @@ exports.capturePayment = async (req, res) => {
 
 // verify the payment or signature 
 
-exports.verifyPayment = async (req, res) => {
-  const razorpay_order_id = req.body?.razorpay_order_id
-  const razorpay_payment_id = req.body?.razorpay_payment_id
-  const razorpay_signature = req.body?.razorpay_signature
-  const courses = req.body?.courses
+exports.verifySignature= async (req, res) => {
+  //get the payment details
+        const {razorpay_payment_id, razorpay_order_id, razorpay_signature} = req.body;
+        const {courses} = req.body;
+        const userId = req.user.id;
+  
+   if(!razorpay_payment_id || !razorpay_order_id || !razorpay_signature) {
+            return res.status(400).json({
+                success:false,
+                message:'Payment details are incomplete',
+            });
+        }
+  
 
-  const userId = req.user.id
+  let body = razorpay_order_id + "|" + razorpay_payment_id;
 
-  if (
-    !razorpay_order_id ||
-    !razorpay_payment_id ||
-    !razorpay_signature ||
-    !courses ||
-    !userId
-  ) {
-    return res.status(200).json({ success: false, message: "Payment Failed" })
-  }
-
-  let body = razorpay_order_id + "|" + razorpay_payment_id
-
-  const expectedSignature = crypto
-    .createHmac("sha256", process.env.RAZORPAY_SECRET)
-    .update(body.toString())
-    .digest("hex")
+  try{
+       //verify the signature
+    
+  const generatedSignature = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET).update(body.toString()).digest("hex");
 
   if (expectedSignature === razorpay_signature) {
-    await enrollStudents(courses, userId, res)
+    await enrollStudents(courses, userId, res);
     return res.status(200).json({ success: true, message: "Payment Verified" })
   }
 
